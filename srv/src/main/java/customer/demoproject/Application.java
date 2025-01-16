a4aec2817d8cd06bfdc3d377bbafcdde8ca21a28
Mayank Sirothia <m.sirothia@sap.com>
2025-01-16 07:51:59 +0000

    Sample commit for Flora

--- srv/src/main/java/customer/demoproject/Application.java
+++ srv/src/main/java/customer/demoproject/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	//Sample commit for Flora Project
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
